3edddc29b4d23af44a59b1fd751450e3579c6672
adrian_cerdeira <acwebdesign01@gmail.com>
2019-12-15 14:37:46 +0100

    -Config erstellt -Daten mit Bild-Darstellung übertragen

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,11 @@
 import * as express from 'express';
+import * as products from './products.json';
 
 const router = express.Router();
 
 // GET: index.html - Übersicht
 router.get('/', (req, res) => {
-    res.render('html/index');
+    res.render('html/index', { products: products });
 });
 
 // GET: shopping-cart.html - Warenkorb
