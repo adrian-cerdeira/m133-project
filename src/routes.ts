9ade97c07eb20193f194b82066d17e4906461e0b
adrian_cerdeira <acwebdesign01@gmail.com>
2019-12-19 20:54:34 +0100

    create dynamic cart with data table and pseudo code for post

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,9 @@
 import * as express from 'express';
 import * as products from './products.json';
+import { Cart } from '../lib/Cart';
 
 const router = express.Router();
+const cart = new Cart();
 
 // GET: index.html - Übersicht
 router.get('/', (req, res) => {
@@ -10,7 +12,7 @@ router.get('/', (req, res) => {
 
 // GET: shopping-cart.html - Warenkorb
 router.get('/cart', (req, res) => {
-    res.render('html/cart');
+    res.render('html/cart', { products: cart.products, total: cart.getTotal() });
 });
 
 // GET: checkout.html - Einkauf abschliessen
@@ -24,5 +26,18 @@ router.get('/products/:id', (req, res) => {
     const selectedProduct = products.find(p => p.id === id);
     res.render('html/product', { product: selectedProduct });
 });
+
+// POST: Produkt zu cart hinzufügen
+/*router.post('/products/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedProduct = products.find(p => p.id === id);
+
+    cart.add(selectedProduct);
+    cart.calculateTotal();
+
+    const total = cart.getTotal();
+
+    res.render('html/cart', { products: cart.products, total: total })
+});*/
 
 module.exports = router;
