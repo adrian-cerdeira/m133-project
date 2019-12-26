0d1fccd8a9f88d31d389f263345b4a7262428e1a
adrian_cerdeira <acwebdesign01@gmail.com>
2019-12-26 20:06:34 +0100

    format code

--- src/routes.ts
+++ src/routes.ts
@@ -7,27 +7,46 @@ const cart = new Cart();
 
 // GET: index.html - Übersicht
 router.get('/', (req, res) => {
-    res.render('html/index', { products: products, total: cart.getTotal() });
+    res.render('html/index',
+        {
+            products: products,
+            total: cart.getTotal()
+        }
+    );
 });
 
 // GET: shopping-cart.html - Warenkorb
 router.get('/cart', (req, res) => {
-    res.render('html/cart', { products: cart.products, total: cart.getTotal() });
+    res.render('html/cart',
+        {
+            products: cart.products,
+            total: cart.getTotal()
+        }
+    );
 });
 
 // GET: checkout.html - Einkauf abschliessen
 router.get('/checkout', (req, res) => {
-    res.render('html/checkout', { total: cart.getTotal() });
+    res.render('html/checkout',
+        {
+            total: cart.getTotal()
+        }
+    );
 });
 
 // GET: product.html - Produkt - Übersicht
 router.get('/products/:id', (req, res) => {
     const id = req.params.id;
     const selectedProduct = products.find(p => p.id === id);
-    res.render('html/product', { product: selectedProduct, total: cart.getTotal() });
+    res.render('html/product',
+        {
+            product: selectedProduct,
+            total: cart.getTotal()
+        }
+    );
 });
 
-// GET: Produkt zu cart hinzufügen
+// GET: cart.html - Produkt zu cart hinzufügen
 router.get('/cart/products/:id', (req, res) => {
     const id = req.params.id;
     const selectedProduct = products.find(p => p.id === id);
@@ -35,7 +54,12 @@ router.get('/cart/products/:id', (req, res) => {
     cart.add(selectedProduct);
     cart.calculateTotal();
 
-    res.render('html/cart', { products: cart.products, total: cart.getTotal() })
+    res.render('html/cart',
+        {
+            products: cart.products,
+            total: cart.getTotal()
+        }
+    );
 });
 
 module.exports = router;
