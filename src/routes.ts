a3554d55e9541f40a3a561791d375f52a23d9cb5
adrian_cerdeira <acwebdesign01@gmail.com>
2019-12-26 20:03:15 +0100

    Total wird im Layout angezeigt

--- src/routes.ts
+++ src/routes.ts
@@ -7,7 +7,7 @@ const cart = new Cart();
 
 // GET: index.html - Übersicht
 router.get('/', (req, res) => {
-    res.render('html/index', { products: products });
+    res.render('html/index', { products: products, total: cart.getTotal() });
 });
 
 // GET: shopping-cart.html - Warenkorb
@@ -17,14 +17,14 @@ router.get('/cart', (req, res) => {
 
 // GET: checkout.html - Einkauf abschliessen
 router.get('/checkout', (req, res) => {
-    res.render('html/checkout');
+    res.render('html/checkout', { total: cart.getTotal() });
 });
 
 // GET: product.html - Produkt - Übersicht
 router.get('/products/:id', (req, res) => {
     const id = req.params.id;
     const selectedProduct = products.find(p => p.id === id);
-    res.render('html/product', { product: selectedProduct });
+    res.render('html/product', { product: selectedProduct, total: cart.getTotal() });
 });
 
 // GET: Produkt zu cart hinzufügen
